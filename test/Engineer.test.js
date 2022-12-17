79514666cbfde5e81705bb3a840a0ad9a43e9adb
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2022-12-17 12:27:48 -0500

    added the tests and made sure they ran properly. Recorded a video showing how everything got generated

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,4 +1,4 @@
-const Engineer = require("../lib/Engineer");
+const Engineer = require("../company/Engineer");
 
 //testing the constructor arguments
 test("Can set GitHUb account via constructor", () => {
--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,4 +1,4 @@
-const Intern = require("../lib/Intern");
+const Intern = require("../company/Intern");
 
 //testing the constructor arguments
 test("Can set school via constructor", () => {
